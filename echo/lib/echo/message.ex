1dbc7415ed2b3c9405d5624918fd19b4276f256d
bsubei <6508762+bsubei@users.noreply.github.com>
2025-03-22 07:06:26 -0400

    encode/decode messages using Poison

--- echo/lib/echo/message.ex
+++ echo/lib/echo/message.ex
@@ -9,20 +9,37 @@ defmodule Echo.Message do
 
   @all_keys [:src, :dest, :body]
   @enforce_keys @all_keys
+  @derive [Poison.Encoder]
   defstruct @all_keys
 
   @type t :: %__MODULE__{
-          src: String.t(),
-          dest: String.t(),
+          src: Types.node_id_t(),
+          dest: Types.node_id_t(),
           body: Body.t()
         }
 
   defmodule Body do
     @type t :: Init.t() | InitOk.t() | Echo.t() | EchoOk.t() | Error.t()
 
+    @doc ~S"""
+      This is where we define the mapping between a body message's type as a string and the corresponding module.
+    """
+    @spec type_str_to_module(String.t()) :: t()
+    def type_str_to_module(type) do
+      case type do
+        "init" -> Body.Init
+        "init_ok" -> Body.InitOk
+        "echo" -> Body.Echo
+        "echo_ok" -> Body.EchoOk
+        "error" -> Body.Error
+        _ -> raise "Unknown message type: #{type}"
+      end
+    end
+
     defmodule Init do
       @all_keys [:type, :msg_id, :node_id, :node_ids]
       @enforce_keys @all_keys
+      @derive [Poison.Encoder]
       defstruct @all_keys
 
       @type t :: %__MODULE__{
@@ -36,6 +53,7 @@ defmodule Echo.Message do
     defmodule InitOk do
       @all_keys [:type, :in_reply_to]
       @enforce_keys @all_keys
+      @derive [Poison.Encoder]
       defstruct @all_keys
 
       @type t :: %__MODULE__{
@@ -47,6 +65,7 @@ defmodule Echo.Message do
     defmodule Echo do
       @all_keys [:type, :echo, :msg_id]
       @enforce_keys @all_keys
+      @derive [Poison.Encoder]
       defstruct @all_keys
 
       @type t :: %__MODULE__{
@@ -61,6 +80,7 @@ defmodule Echo.Message do
       @optional_keys [msg_id: nil]
       @all_keys @mandatory_keys ++ @optional_keys
       @enforce_keys @mandatory_keys
+      @derive [Poison.Encoder]
       defstruct @all_keys
 
       @type t :: %__MODULE__{
@@ -74,6 +94,7 @@ defmodule Echo.Message do
     defmodule Error do
       @all_keys [:type, :in_reply_to, :code, :text]
       @enforce_keys @all_keys
+      @derive [Poison.Encoder]
       defstruct @all_keys
 
       @type t :: %__MODULE__{
@@ -83,5 +104,44 @@ defmodule Echo.Message do
               text: String.t()
             }
     end
+  end
+
+  @doc ~S"""
+    Decodes the given JSON-encoded string into a Message object.
+
+    ## Examples
+
+        iex> iodata = ~s({"src":"c0","dest":"n1","body":{"type":"error","text":"dunno","in_reply_to":2,"code":42}})
+        iex> %Echo.Message{src: "c0", dest: "n1", body: %Echo.Message.Body.Error{type: "error", in_reply_to: 2, code: 42, text: "dunno"}} = Echo.Message.decode(iodata)
+
+  """
+  @spec decode(iodata()) :: Echo.Message.t()
+  def decode(input) do
+    # First decode to a plain map with atom keys so we can pass them into struct().
+    map = Poison.decode!(input, keys: :atoms!)
+
+    # Then manually create the body struct with the required fields
+    body_map = map[:body]
+    body_type = Body.type_str_to_module(body_map[:type])
+    body_struct = struct(body_type, body_map)
+
+    # Create the final struct with all required fields
+    struct(__MODULE__, src: map[:src], dest: map[:dest], body: body_struct)
+  end
+
+  @doc ~S"""
+    Encodes the given Message object into a JSON-encoded string.
+
+    ## Examples
+
+        iex> msg = %Echo.Message{src: "c0", dest: "n1", body: %Echo.Message.Body.Error{type: "error", in_reply_to: 2, code: 42, text: "dunno"}}
+        iex> ~s({"src":"c0","dest":"n1","body":{"type":"error","text":"dunno","in_reply_to":2,"code":42}}) = Echo.Message.encode(msg)
+        iex> msg = %Echo.Message{src: "c0", dest: "n1", body: %Echo.Message.Body.Echo{type: "echo", echo: "well hello there", msg_id: 555}}
+        iex> ~s({"src":"c0","dest":"n1","body":{"type":"echo","msg_id":555,"echo":"well hello there"}}) = Echo.Message.encode(msg)
+
+  """
+  @spec encode(Echo.Message.t()) :: binary()
+  def encode(message) do
+    Poison.encode!(message)
   end
 end
