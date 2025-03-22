f1419e150ea38e9b86bde76a6e48160fad9bc482
bsubei <6508762+bsubei@users.noreply.github.com>
2025-03-22 11:38:31 -0400

    read and parse init message, still need to reply

--- echo_server/lib/echo_server/node.ex
+++ echo_server/lib/echo_server/node.ex
@@ -1,4 +1,18 @@
 defmodule EchoServer.Node do
-  @type t :: %__MODULE__{}
-  defstruct []
+  alias EchoServer.Message.Types
+
+  @all_keys [:node_id, :node_ids, :current_msg_id]
+  @enforce_keys @all_keys
+  defstruct @all_keys
+
+  @type t :: %__MODULE__{
+          node_id: Types.node_id_t() | nil,
+          node_ids: list(Types.node_id_t()) | nil,
+          current_msg_id: Types.msg_id_t()
+        }
+
+  @spec new() :: t()
+  def new() do
+    %__MODULE__{node_id: nil, node_ids: nil, current_msg_id: 0}
+  end
 end
--- echo_server/lib/echo_server/server.ex
+++ echo_server/lib/echo_server/server.ex
@@ -5,8 +5,11 @@ defmodule EchoServer.Server do
   use GenServer
   require Logger
 
-  @type t :: %__MODULE__{buffer: binary(), node_state: Node.t()}
-  defstruct [:buffer, :node_state]
+  # TODO use a byte buffer to handle incomplete messages.
+  @all_keys [:node_state]
+  @enforce_keys @all_keys
+  defstruct @all_keys
+  @type t :: %__MODULE__{node_state: Node.t()}
 
   # Public API exposed to the client
   @spec start_link(keyword()) :: GenServer.on_start()
@@ -17,8 +20,11 @@ defmodule EchoServer.Server do
   # Internal API for server callbacks
   @impl true
   def init(_) do
+    IO.puts(:stderr, "Starting up EchoServer")
+
     send(self(), :read_stdin)
-    {:ok, %__MODULE__{}}
+
+    {:ok, %__MODULE__{node_state: Node.new()}}
   end
 
   @impl true
@@ -35,19 +41,23 @@ defmodule EchoServer.Server do
   @spec handle_input(t(), binary()) :: t()
   def handle_input(state, input) do
     message = Message.decode(input)
+    IO.inspect(:stderr, message, label: "received message")
 
-    state =
-      case message do
-        %Body.Init{} = init ->
-          # TODO do something
-          IO.inspect(init)
+    case message.body do
+      %Body.Init{} ->
+        node_id = message.body.node_id
+        node_ids = message.body.node_ids
+
+        # TODO reply with InitOk
+
+        # Store the node_id and node_ids into our Node state.
+        %__MODULE__{
           state
-
-        unknown ->
-          IO.inspect(unknown)
-          state
-      end
-
-    state
+          | node_state:
+              update_in(state.node_state, fn st ->
+                put_in(st.node_id, node_id) |> put_in(st.node_ids, node_ids)
+              end)
+        }
+    end
   end
 end
