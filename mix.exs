25371f86cab3980198885e938ae400ecdc47417f
bsubei <6508762+bsubei@users.noreply.github.com>
2025-03-24 08:30:54 -0400

    update project setup to allow for launching different escripts

--- mix.exs
+++ mix.exs
@@ -1,14 +1,16 @@
 defmodule MaelstromTutorial.MixProject do
   use Mix.Project
 
+  @main_module_mapping %{"echo" => MaelstromTutorial.EchoServer.Server}
+
   def project do
     [
-      app: :echo,
+      app: :maelstrom_tutorial,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: MaelstromTutorial.EchoServer.Server, embed_elixir: true]
+      escript: escript_config()
     ]
   end
 
@@ -18,6 +20,21 @@ defmodule MaelstromTutorial.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:poison, "~> 6.0"}
+    ]
+  end
+
+  defp escript_config do
+    module_name = System.get_env("MODULE", "echo")
+
+    main_module =
+      Map.get(@main_module_mapping, module_name) ||
+        raise("Could not find main_module matching '#{module_name}'")
+
+    [
+      main_module: main_module,
+      name: module_name,
+      embed_elixir: true,
+      app: nil
     ]
   end
 end
--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-set -e
 
 MAELSTROM="./maelstrom/maelstrom"
-BINARY="./echo"
-mix escript.build
+
+# Install maelstrom if it doesn't exist. install-maelstrom is defined in .devenv file.
+ls "${MAELSTROM}" > /dev/null 2>&1
+exit_code=$?
+if [ "${exit_code}" -ne 0 ]; then
+  install-maelstrom
+fi
+
+set -e  # Bail on failures after this point
+
+BINARY="./${1}"
+
+MODULE="${1}" mix escript.build
 
 if [ "$1" = "echo" ]; then
   $MAELSTROM test -w echo --bin "${BINARY}" --time-limit 5 --log-stderr --node-count 1 --rate 100 --latency 120
