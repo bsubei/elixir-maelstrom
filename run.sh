d571fd61ebf7cafc058a0e6fb7b853f796c331d0
bsubei <6508762+bsubei@users.noreply.github.com>
2025-03-24 07:11:47 -0400

    change dir structure of project, remove unnecessary nesting dir

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ mix escript.build
 
 if [ "$1" = "echo" ]; then
   $MAELSTROM test -w echo --bin "${BINARY}" --time-limit 5 --log-stderr --node-count 1 --rate 100 --latency 120
+elif [ "$1" = "broadcast" ]; then
+  $MAELSTROM test -w broadcast --bin "${BINARY}" --time-limit 5 --log-stderr
 else
   echo "unknown command: '$1'"
 fi
